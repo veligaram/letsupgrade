1e9d7383ccd471d4407f5715c0615a0896fccb0d
Devanandh <100019332+veligaram@users.noreply.github.com>
2023-07-25 18:55:35 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,13 @@
-
+marks=int(input())
+if marks>=90:
+  print("grade: ","A+")
+elif marks>=80 and marks<90:
+  print("grade: ","A")
+elif marks>=70 and marks<80:
+  print("grade: ","B")
+elif marks>=60 and marks<70:
+  print("grade: ","C")
+elif marks>=50 and marks<60:
+  print("grade: ","D")
+else:
+  print("Fail")
