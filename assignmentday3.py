72ba9c8239318ffa069ec1b69bc858a56269e11f
Devanandh <100019332+veligaram@users.noreply.github.com>
2023-07-26 19:17:13 +0530

    Update assignmentday3.py

--- assignmentday3.py
+++ assignmentday3.py
@@ -1,1 +1,20 @@
+class Animal:
+    def make_sound(self):
+        print("Generic animal sound")
+class Dog(Animal):
+    def make_sound(self):
+        print("Bark")
+
+class Cat(Animal):
+    def make_sound(self):
+        print("Meow")
+def animal_sound_in_zoo(animal):
+    animal.make_sound()
+
+if __name__ == "__main__":
+    dog_instance = Dog()
+    cat_instance = Cat()
+
+    animal_sound_in_zoo(dog_instance)  
+    animal_sound_in_zoo(cat_instance)  
 
